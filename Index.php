7ee5bcddce5204a39f5e415c8f673df5bd66ae48
varshitha kudupudi <varshitha.kud@gmail.com>
2024-11-13 22:56:50 +0530

    Added index

---
+++ Index.php
@@ -0,0 +1,17 @@
+<?php
+$name = "varshitha";
+$month = 10;
+if ($month==7){
+    echo $name;
+}
+else if ($month==3){
+    echo "harshitha";
+}
+else if ($month==10){
+    echo "harini";
+}
+while ($month < 20){
+    echo $month ."<br>";
+    $month++;
+}
+?>
